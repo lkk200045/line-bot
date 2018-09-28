39a9648635819ec41aacc7dff72369fefbd372a9
Huang Steve <lkk200045@gmail.com>
2018-09-28 16:58:40 +0800

    app.py

--- organize.py
+++ organize.py
@@ -1,5 +1,4 @@
 #SDK 載入LINE SDK
-import requests
 
 from flask import Flask, request, abort
 
@@ -13,30 +12,11 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-
-
-
-
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('/sjBLgjHsNZhdsV+Xy9pXu7rPIrErYLvvbLfVOEYDyaiH3IEVROEnEYrMkPF+BuGCFjbTu3HSfTSUfVTJz6rLIWluhYeZp7v5FKZa94SF7pkcCPvY7El21pJuki1kpg5gl8QLxtGEfhtSutfmxdgUgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('ba4bdf20d14b1338b998a01491aa691f')
 
-
-headers = {
-    # Request headers
-    'Ocp-Apim-Subscription-Key': '5cf8bae594b24645bc0971c7b1169ed9',
-}
-
-params ={
-    # Query parameter
-    'q': '我愛你',
-    # Optional request parameters, set to default values
-    'timezoneOffset': '0',
-    'verbose': 'false',
-    'spellCheck': 'false',
-    'staging': 'false',
-}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -59,25 +39,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    params['q'] = msg
-    r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/d9f3feb1-6cf3-4f39-8821-e6c2bbb86fc6',headers=headers, params=params)
-    result = r.json()
-    a = result['topScoringIntent']['intent']
-
-    if a == '告白':
-        s = '謝謝我不愛妳'
-    if a == '問候':
-        s = '早安，慈濟歡迎您'
-    if a == '誇獎':
-        s = '就算你誇獎我我也不會開心的 (害羞)'
-    if a == '隱私':
-        s = '不告訴你!!'
-    else:
-        s = '我聽不懂' 
-
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=msg))
 
 
 if __name__ == "__main__":
