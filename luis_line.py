5f0db5675fff024db7ba58ac2b90d4b0be7e114a
Huang Steve <lkk200045@gmail.com>
2018-11-26 11:49:30 +0800

    app.py

---
+++ luis_line.py
@@ -0,0 +1,92 @@
+#SDK 載入LINE SDK
+import requests
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    ImageSendMessage,LocationMessage,TemplateSendMessage, ButtonsTemplate, URITemplateAction,
+    PostbackTemplateAction, MessageTemplateAction, CarouselTemplate, CarouselColumn, ConfirmTemplate
+)
+
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '5cf8bae594b24645bc0971c7b1169ed9',
+}
+
+params ={
+    # Query parameter
+    'q': '我愛你',
+    # Optional request parameters, set to default values
+    'timezoneOffset': '0',
+    'verbose': 'false',
+    'spellCheck': 'false',
+    'staging': 'false',
+}
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('/sjBLgjHsNZhdsV+Xy9pXu7rPIrErYLvvbLfVOEYDyaiH3IEVROEnEYrMkPF+BuGCFjbTu3HSfTSUfVTJz6rLIWluhYeZp7v5FKZa94SF7pkcCPvY7El21pJuki1kpg5gl8QLxtGEfhtSutfmxdgUgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('ba4bdf20d14b1338b998a01491aa691f')
+
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '5cf8bae594b24645bc0971c7b1169ed9',
+}
+
+def luis(query):
+    params['q'] = query
+    r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/d9f3feb1-6cf3-4f39-8821-e6c2bbb86fc6',headers=headers, params=params)
+    result = r.json()
+    a = result['topScoringIntent']['intent']
+    return a
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    msg = msg.encode('utf-8')
+    a = luis(msg)
+    if a =='告白':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:告白 回應:我不愛妳'))
+    elif a=='聊天':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:聊天 回應:我不想聊天'))
+    elif a=='例外':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:例外 回應:我聽不懂耶'))
+    elif a=='詢問':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:詢問 回應:歡迎光臨'))
+    elif a=='誇獎':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:誇獎 回應:我會不好意思耶'))
+    elif a=='問候':
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:問候 回應:你好喔'))
+    else :
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:不明 回應:可以請你換句話說嗎?'))
+       
+if __name__ == "__main__":
+    app.run()
