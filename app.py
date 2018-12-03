11d25c6583fb67acfd3d7c444f9f0cabf85ca7e1
Huang Steve <lkk200045@gmail.com>
2018-12-03 22:57:18 +0800

    app.py

--- app.py
+++ app.py
@@ -74,9 +74,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    msg = msg.encode('utf-8')
-    a = luis(msg)
+    a = luis(event.message.text)
     if a =='告白':
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:告白 回應:我不愛妳'))
     elif a=='聊天':
