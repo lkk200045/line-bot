75bc1e6670e34231ba11adbd017253f60c2ba243
Huang Steve <lkk200045@gmail.com>
2019-01-02 15:46:22 +0800

    app.py

--- app.py
+++ app.py
@@ -157,235 +157,41 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:誇獎 回應:我會不好意思耶'))
     elif a=='問候':
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:問候 回應:你好喔'))
-    elif a == "我要找工作" :
+    elif a == "高雄" :
         Carousel_template = TemplateSendMessage(
             alt_text='Carousel template',
             template=CarouselTemplate(
                 columns=[
                 CarouselColumn(
-                    thumbnail_image_url='https://www.104.com.tw/jobs/main/static/img/fb_600x315.png',
-                    title='以職務類別區分',
-                    text='',
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/NSYSU_MS05.JPG/460px-NSYSU_MS05.JPG',
+                    title='中山跑腿小弟',
+                    text='幫忙外送飲料，詳細地點高雄中山大學，時薪200',
                     actions=[
                     MessageTemplateAction(
-                        label='104歡迎您',
-                        text='選擇職務類別區分'
+                        label='中山跑腿小弟',
+                        text='中山跑腿小弟'
                     )
                 ]
             ),
                 CarouselColumn(
                     thumbnail_image_url='https://rakumatw.r10s.com/d/strg/ctrl/27/1852d4cee0e9540099c5db2f1b99936027ffdac2.60.1.27.2.jpg',
-                    title='以地區區分',
-                    text='',
+                    title='接小孩',
+                    text='幫忙接小孩，詳細地點鹽埕區五福四路100號，時薪200',
                     actions=[
                     MessageTemplateAction(
-                        label='104歡迎您',
-                        text='選擇地區'
+                        label='接小孩',
+                        text='接小孩'
                     )
                 ]
             ),
              CarouselColumn(
                     thumbnail_image_url='https://static.juksy.com/files/articles/68605/5a35353b09a3d.jpg',
-                    title='以工作性質區分',
-                    text='',
+                    title='正妹求搬家',
+                    text='幫忙搬家，詳細地點鹽埕區五福四路1號，友情無價，陪你吃頓飯',
                     actions=[
                     MessageTemplateAction(
-                        label='104歡迎您',
-                        text='選擇工作性質'
-                    )
-                ]
-            ),
-        ]
-        )
-            )
-        line_bot_api.reply_message(event.reply_token,Carousel_template)
-    elif a == "正妹求搬家" :
-        Confirm_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ConfirmTemplate(
-                title='確認',
-                text='您選的資料為正妹求搬家',
-                actions=[                              
-                PostbackTemplateAction(
-                    label='是',
-                    text='是',
-                    data='action=buy&itemid=1'
-                    ),
-                MessageTemplateAction(
-                    label='否',
-                    text='否'
-                    )
-                ]
-            )
-    )
-        line_bot_api.reply_message(event.reply_token,Confirm_template)
-    elif a == "接小孩" :
-        Confirm_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ConfirmTemplate(
-                title='確認',
-                text='您選的資料為接小孩',
-                actions=[                              
-                PostbackTemplateAction(
-                    label='是',
-                    text='是',
-                    data='action=buy&itemid=1'
-                    ),
-                MessageTemplateAction(
-                    label='否',
-                    text='否'
-                    )
-                ]
-            )
-    )
-        line_bot_api.reply_message(event.reply_token,Confirm_template)
-    elif a == "中山跑腿小弟" :
-        Confirm_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ConfirmTemplate(
-                title='確認',
-                text='您選的資料為中山跑腿小弟',
-                actions=[                              
-                PostbackTemplateAction(
-                    label='是',
-                    text='是',
-                    data='action=buy&itemid=1'
-                    ),
-                MessageTemplateAction(
-                    label='否',
-                    text='否'
-                    )
-                ]
-            )
-    )
-        line_bot_api.reply_message(event.reply_token,Confirm_template)
-    elif a == "是" :
-        Confirm_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ConfirmTemplate(
-                title='確認',
-                text='請選擇您的身分',
-                actions=[                              
-                PostbackTemplateAction(
-                    label='學生兼差',
-                    text='學生兼差',
-                    data='action=buy&itemid=1'
-                    ),
-                MessageTemplateAction(
-                    label='上班族兼差',
-                    text='上班族兼差'
-                    )
-                ]
-            )
-    )
-        line_bot_api.reply_message(event.reply_token,Confirm_template)
-    elif a == "否" :
-        Confirm_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ConfirmTemplate(
-                title='確認',
-                text='請選擇您的身分',
-                actions=[                              
-                PostbackTemplateAction(
-                    label='學生兼差',
-                    text='學生兼差',
-                    data='action=buy&itemid=1'
-                    ),
-                MessageTemplateAction(
-                    label='上班族兼差',
-                    text='上班族兼差'
-                    )
-                ]
-            )
-    )
-        line_bot_api.reply_message(event.reply_token,Confirm_template)
-    elif a == '學生兼差' or a == '上班族兼差':
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入姓名'))
-    elif a == '小明' :
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入電話'))
-    elif a == '0987787587' :
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='報名成功 您的序號為001號'))
-    elif a == "我要找人才":
-        buttons_template_message = TemplateSendMessage(
-            alt_text='hi',
-            template=ButtonsTemplate(
-                thumbnail_image_url='https://www.limitlessiq.com/media/catalog/product/cache/1/small_image/200x200/9df78eab33525d08d6e5fb8d27136e95/z/0/z01.jpg',
-                title='請選擇所在城市',
-                text='歡迎光臨',
-                actions=[
-                MessageTemplateAction(
-                    label='高雄人才', text='高雄人才'
-                    ),
-                MessageTemplateAction(
-                    label='台北人才', text='台北人才'
-                    ),
-                MessageTemplateAction(
-                    label='台南人才', text='台南人才'
-                    ),
-                MessageTemplateAction(
-                    label='台中人才', text='台中人才'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(
-            event.reply_token,
-            buttons_template_message)
-    elif a == '高雄人才' :
-        buttons_template_message = TemplateSendMessage(
-            alt_text='hi',
-            template=ButtonsTemplate(
-                thumbnail_image_url='https://www.limitlessiq.com/media/catalog/product/cache/1/small_image/200x200/9df78eab33525d08d6e5fb8d27136e95/z/0/z01.jpg',
-                title='請問有指定專員嗎?',
-                text='您好',
-                actions=[
-                MessageTemplateAction(
-                    label='有', text='有'
-                    ),
-                MessageTemplateAction(
-                    label='沒有', text='沒有'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(
-            event.reply_token,
-            buttons_template_message)
-    elif a == "有": 
-        Carousel_template = TemplateSendMessage(
-            alt_text='Carousel template',
-            template=CarouselTemplate(
-                columns=[
-                CarouselColumn(
-                    thumbnail_image_url='https://cc.tvbs.com.tw/img/upload/2018/08/26/20180826221834-d60f8573.jpg',
-                    title='中山劉德華',
-                    text='搬家界的第一把交椅',
-                    actions=[
-                    MessageTemplateAction(
-                        label='中山劉德華',
-                        text='中山劉德華'
-                    )
-                ]
-            ),
-                CarouselColumn(
-                    thumbnail_image_url='https://cw1.tw/CW/images/article/C1418099648229.jpg',
-                    title='高雄金城武',
-                    text='外賣界的四大天王',
-                    actions=[
-                    MessageTemplateAction(
-                        label='高雄金城武',
-                        text='高雄金城武'
-                    )
-                ]
-            ),
-             CarouselColumn(
-                    thumbnail_image_url='https://fs.mingpao.com/pns/20171215/s00092/5e98a17b98b881b7730d0d6b1e52a14b.jpg',
-                    title='台南林志玲',
-                    text='帶小孩的最美名模',
-                    actions=[
-                    MessageTemplateAction(
-                        label='台南林志玲',
-                        text='台南林志玲'
+                        label='正妹求搬家',
+                        text='正妹求搬家'
                     )
                 ]
             ),
@@ -393,18 +199,6 @@ def handle_message(event):
         )
     )
         line_bot_api.reply_message(event.reply_token,Carousel_template)
-    elif a == "台南林志玲" or a == "高雄金城武"or a == "中山劉德華" or a == "沒有":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入工作資訊'))
-    elif a == "幫忙接小孩":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入地點'))
-    elif a == "中山大學":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入姓名'))
-    elif a == "阿伯":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請輸入電話'))
-    elif a == "0978787587":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='完成報名'))
-    elif a == "客訴服務":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='您好，請輸入您的電話，我們將有專人為您服務。'))
     else :
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='意圖:不明 回應:可以請你換句話說嗎?'))
        
