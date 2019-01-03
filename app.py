2629d73ee5b1d5dc082cca6ac1b001160e329238
Huang Steve <lkk200045@gmail.com>
2019-01-03 11:38:07 +0800

    app.py

--- app.py
+++ app.py
@@ -289,9 +289,9 @@ def handle_message(event):
                     title='104人力銀行',
                     text='您好，歡迎光臨',
                     actions=[
-                    MessageTemplateAction(
-                        label='資訊軟體系統類',
-                        text='資訊軟體系統類'
+                    URITemplateAction(
+                        label='經營/人資類',
+                        uri='https://www.104.com.tw/jobs/search/?cat=2001000000&jobsource=joblist_a_date&ro=0'
                     )
                 ]
             ),
