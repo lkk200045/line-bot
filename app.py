514d505bdeffda69d917ceab86554b4256654b88
Huang Steve <lkk200045@gmail.com>
2019-01-03 11:47:52 +0800

    app.py

--- app.py
+++ app.py
@@ -305,17 +305,6 @@ def handle_message(event):
                         uri='https://www.104.com.tw/job/?jobno=6eol2&jobsource=joblist_a_date'
                     )
                 ]
-            ),
-            CarouselColumn(
-                    thumbnail_image_url='https://www.104.com.tw/jobs/main/static/img/fb_600x315.png',
-                    title='美商動信安全股份有限公司台灣分公司',
-                    text='工作經歷:不拘，薪水7萬以上',
-                    actions=[
-                   URITemplateAction(
-                        label='Sr. Windows Software Engineer(台北)',
-                        uri='https://www.104.com.tw/job/?jobno=6cds0&jobsource=joblist_a_date'
-                    )
-                ]
             ), 
             CarouselColumn(
                     thumbnail_image_url='https://www.104.com.tw/jobs/main/static/img/fb_600x315.png',
